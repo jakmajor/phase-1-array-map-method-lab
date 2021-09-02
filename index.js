59ed6c9b896e55f1297b02640cb0ae9900275d1f
Jak Major <hellojakkaiser@gmail.com>
2021-09-01 19:46:06 -0700

    commit tree 1ff483e305bfcae4d40e7a352dc179cd75a0d8b3

--- index.js
+++ index.js
@@ -15,8 +15,6 @@ const tutorials = [
 
 
 const titleCased = () => {
-  let titleCaseTutorials = [];
-
   return tutorials.map(tutorial => {
     return tutorial.split(' ').map(word => {
     return word.charAt(0).toUpperCase() + word.slice(1)
